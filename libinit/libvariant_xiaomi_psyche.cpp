e9903c5c8c35099356b63bc3b35a956884d1e516
Verevka <verevka86@gmail.com>
2026-03-06 18:15:44 +0300

    psyche: libinit: Drop fingerprint override

--- libinit/libvariant_xiaomi_psyche.cpp
+++ libinit/libvariant_xiaomi_psyche.cpp
@@ -18,7 +18,6 @@ static const variant_info psyche_global_info = {
     .marketname = "Xiaomi 12X",
     .mod_device = "psyche_global",
     .model = "2112123AG",
-    .build_fingerprint = "Xiaomi/psyche_global/psyche:13/TKQ1.221114.001/V816.0.8.0.TLDMIXM:user/release-keys",
     .nfc = true,
 };
 
@@ -34,7 +33,6 @@ static const variant_info psyche_info = {
     .marketname = "Xiaomi 12X",
     .mod_device = "psyche_global",
     .model = "2112123AC",
-    .build_fingerprint = "Xiaomi/psyche/psyche:13/TKQ1.221114.001/V816.0.10.0.TLDCNXM:user/release-keys",
     .nfc = true,
 };
 
